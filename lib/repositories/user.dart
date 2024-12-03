f920ad9ce6438eb8fb726aa9d0c703cbc5305d00
Salma Elsoly <salmaelsoly@gmail.com>
2024-12-03 19:40:19 +0200

    refactor syncing of friends

--- lib/repositories/user.dart
+++ lib/repositories/user.dart
@@ -16,7 +16,7 @@ class UserRepository {
     }
   }
 
-  Future<void> loginUser(String firestoreId)async {
+  Future<void> loginUser(String firestoreId) async {
     try {
       UserModel loggedIn = await _firestore.getUser(firestoreId);
       await _localDB.insertUser(loggedIn);
@@ -27,41 +27,58 @@ class UserRepository {
 
   Future<List<UserModel>> getFriends(String userId) async {
     try {
+      final localFriends = await _localDB.getFriendOfUser(userId);
+      var friends = localFriends.map((friend) => UserModel.fromMap(friend)).toList();
+
       // Check internet connection
       final connectivityResult = await Connectivity().checkConnectivity();
       final isConnected = connectivityResult != ConnectivityResult.none;
 
       if (isConnected) {
-        final friendsRemote = await _firestore.getFriendsOfUser(userId);
-        await _localDB.deleteFriendsOfUser(userId);
-
-        for (var friendRemote in friendsRemote) {
-          await _localDB.insertUser(friendRemote);
-        }
+        final remoteFriends = await _firestore.getFriendsOfUser(userId);
 
 
-        final friendsLocal = await _localDB.getFriendOfUser(userId);
-        return friendsLocal.map((friend) => UserModel.fromMap(friend)).toList();
-      } else {
-        final friendsLocal = await _localDB.getFriendOfUser(userId);
-        return friendsLocal.map((friend) => UserModel.fromMap(friend)).toList();
+        await _syncFriends(userId, remoteFriends);
+
+
+        friends = remoteFriends;
       }
+
+      return friends;
     } catch (e) {
       rethrow;
     }
   }
+  Future<void> _syncFriends(String userId, List<UserModel> remoteFriends) async {
+    final localFriends = await _localDB.getFriendOfUser(userId);
 
-  Future<UserModel> getUser(String id) async {
-      try {
-        final userMap = await _localDB.getUserByFirestoreId(id);
-        if (userMap != null) {
-          return UserModel.fromMap(userMap);
+    final localMap = {for (var friend in localFriends) friend['firestoreId']: friend};
+
+    // Determine which friends to add or update
+    for (var remoteFriend in remoteFriends) {
+      final localFriend = localMap[remoteFriend.firestoreId];
+      if (localFriend == null) {
+        // Add new friend to local DB
+        await _localDB.insertUser(remoteFriend);
+      } else {
+        // Update friend if remote data is newer
+        if (DateTime.parse(remoteFriend.lastModified.toString()).isAfter(
+            DateTime.parse(localFriend['lastModified']))) {
+          await _localDB.updateUser(remoteFriend);
         }
-        throw Exception('No user found or saved');
-      } catch (e) {
-        rethrow;
       }
     }
+  }
 
-
+  Future<UserModel> getUser(String id) async {
+    try {
+      final userMap = await _localDB.getUserByFirestoreId(id);
+      if (userMap != null) {
+        return UserModel.fromMap(userMap);
+      }
+      throw Exception('No user found or saved');
+    } catch (e) {
+      rethrow;
+    }
+  }
 }
