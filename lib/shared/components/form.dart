72e136dcdc56cdff2d9c77a8e5558d070a30f0a6
Salma Elsoly <salmaelsoly@gmail.com>
2024-12-13 01:28:45 +0200

    fix form field color for dark theme

--- lib/shared/components/form.dart
+++ lib/shared/components/form.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../theme.dart';
 
 Widget defaultFormField({
   required TextEditingController controller,
@@ -43,7 +46,7 @@ Widget defaultFormField({
               borderSide:
                   BorderSide(color: Theme.of(context).colorScheme.secondary)),
           filled: true,
-          fillColor: Theme.of(context).colorScheme.onSecondary.withOpacity(0.7),
+          fillColor: Provider.of<ThemeColorData>(context, listen: false).isDark?Colors.black38:Theme.of(context).colorScheme.onSecondary.withOpacity(0.7),
           prefixIcon:
               Icon(prefix, color: Theme.of(context).colorScheme.secondary),
           suffixIcon: suffix != null
