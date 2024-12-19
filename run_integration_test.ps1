c79cc28f770566143c18d91ea30115035c77d45e
Salma Elsoly <salmaelsoly@gmail.com>
2024-12-19 22:21:11 +0200

    add integration test

---
+++ run_integration_test.ps1
@@ -0,0 +1,73 @@
+
+param (
+    [string]$apkPath = ".\build\app\outputs\flutter-apk\app-debug.apk",
+    [string]$testDriver = "test_driver/integration_test.dart",
+    [string]$testTarget = "integration_test/app_test.dart",
+    [string]$resultsDirectory = ".\integration_test_result",
+    [string]$logFile = ".\integration_test_result\test_log.txt",
+    [string]$recordedScreenFile = ".\integration_test_result\test_screen.mp4",
+    [string]$genHtmlPath = "C:\ProgramData\chocolatey\lib\lcov\tools\bin\genhtml"
+)
+
+
+if (-Not (Test-Path -Path $resultsDirectory)) {
+    New-Item -ItemType Directory -Path $resultsDirectory
+}
+
+
+if (Test-Path -Path $logFile) {
+    Remove-Item -Path $logFile
+}
+if (Test-Path -Path $recordedScreenFile) {
+    Remove-Item -Path $recordedScreenFile
+}
+if (Test-Path -Path ".\coverage\html") {
+    Remove-Item -Path ".\coverage\html" -Recurse
+}
+
+
+Write-Host "Installing APK on the connected device..."
+adb install -r $apkPath
+
+
+Write-Host "Initiating screen recording on the device..."
+$screenRecordingJob = Start-Job -ScriptBlock {
+    adb shell screenrecord /data/local/tmp/test_screen.mp4
+}
+Start-Sleep -Seconds 5
+
+
+Write-Host "Running Flutter integration tests..."
+try {
+    flutter test --coverage integration_test/
+} catch {
+    Write-Host "Tests encountered errors. Check the log file for details."
+}
+
+
+Write-Host "Ending screen recording on the device..."
+Stop-Job -Job $screenRecordingJob
+Wait-Job -Job $screenRecordingJob
+Start-Sleep -Seconds 5
+
+
+Write-Host "Retrieving the recorded screen file from the device..."
+adb pull /data/local/tmp/test_screen.mp4 $recordedScreenFile
+
+
+Write-Host "Generating test coverage report..."
+try {
+    if (-Not (Test-Path -Path ".\coverage\html")) {
+        New-Item -ItemType Directory -Path ".\coverage\html"
+    }
+    perl $genHtmlPath coverage/lcov.info --output-directory coverage/html
+    Get-Content .\coverage\lcov.info | Out-File -FilePath $resultsDirectory\coverage_report.txt
+} catch {
+    Write-Host "Could not generate the coverage report. Check if genhtml is properly installed."
+}
+
+
+Write-Host "Cleaning up temporary files from the device..."
+adb shell rm /data/local/tmp/test_screen.mp4
+
+Write-Host "Test process completed! Logs, screen recording, and coverage reports are saved in $resultsDirectory"
